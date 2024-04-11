adda44e852d6bcc60e63892d556cffff9826ce6d
zdodson21 <zdodson21@gmail.com>
2024-04-10 21:21:42 -0400

    made small comment

--- src/my-social-media.js
+++ src/my-social-media.js
@@ -1,4 +1,5 @@
 /**
  * This would essentially be a web component that will display the user's selected social media icons (chosen using attributes) and when clicked, will take them to their social media page (via link in an attribute)
+ * Might make open source (:D)
  */
 

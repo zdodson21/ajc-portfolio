ed2833568ae14895a33cc35031c4ac0338ded79e
zdodson21 <zdodson21@gmail.com>
2024-04-11 16:24:02 -0400

    added video and youtube video capability

--- src/picture-frame.js
+++ src/picture-frame.js
@@ -11,29 +11,67 @@ export class PictureFrame extends LitElement {
 
     constructor() {
         super();
-        this.imgLocation = '';
+        this.displayLocation = '';
         this.src = '';
         this.alt = '';
         this.caption = '';
         this.href = '';
         this.anchorText = '';
+        this.type = '';
         this.border = true;
     }
 
     static get styles() {
         return css`
-            
+
+
+            :host([type='image']) .frame-image img{
+                display: content;
+            } :host([type='image']) .frame-image video{
+                display: none;
+            } :host([type='image']) .frame-image iframe{
+                display: none;
+            }
+
+            :host([type='video']) .frame-image img{
+                display: none;
+            } :host([type='video']) .frame-image video{
+                display: content;
+            } :host([type='video']) .frame-image iframe{
+                display: none;
+            }
+
+            :host([type='youtube']) .frame-image img{
+                display: none;
+            } :host([type='youtube']) .frame-image video{
+                display: none;
+            } :host([type='youtube']) .frame-image iframe{
+                display: content;
+            }
+
             :host([border]) .img-container {
                 border-style: var(--ajc-border-default-frame1);
+            }
+
+            .picture-frame-container {
+                display: flex;
+                justify-content: center;
+                
             }
 
             .img-container {
                 display: flex;
                 padding: var(--ajc-spacing-default-4);
+                margin: var(--ajc-spacing-default-7) var(--ajc-spacing-default-0);
+                width: 920px;
             }
 
             .img-container .frame-item {
                 width: 100%;
+            }
+
+            .frame-image video {
+                width: 360px;
             }
 
             :host([href = '']) a {
@@ -45,11 +83,15 @@ export class PictureFrame extends LitElement {
     }
 
     render() {
-        if (this.imgLocation === 'left' || this.imgLocation === 'l') {
+        if (this.type !== 'image' && this.type !== 'video' && this.type !== 'youtube') {
+            return html `
+                <p><strong> Please set the type attribute to 'image', 'video', or 'youtube'</strong></p>
+            `
+        } else if (this.displayLocation === 'left' || this.displayLocation === 'l') {
             return this.imageLeft();
-        } else if (this.imgLocation === 'right' || this.imgLocation === 'r') {
+        } else if (this.displayLocation === 'right' || this.displayLocation === 'r') {
             return this.imageRight();
-        } else if (this.imgLocation === 'top' || this.imgLocation === 't') {
+        } else if (this.displayLocation === 'top' || this.displayLocation === 't') {
             return this.imageTop();
         } else {
             return html`
@@ -63,13 +105,17 @@ export class PictureFrame extends LitElement {
      */
     imageLeft() {
         return html`
-            <div class='img-container img-container-left'>
-                <div class='frame-item frame-image'>
-                    <img src="${this.src}" alt="${this.alt}">
-                </div>    
-                <div class='frame-item frame-text'>
-                    <p>${this.caption}</p>
-                    <a href="${this.href}">${this.anchorText}</a>
+            <div class='picture-frame-container'>
+                <div class='img-container img-container-left'>
+                    <div class='frame-item frame-image'>
+                        <img src="${this.src}" alt="${this.alt}">
+                        <video src="${this.src}" controls></video>
+                        <iframe width="360" height="202.5" src="${this.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>    
+                    <div class='frame-item frame-text'>
+                        <p>${this.caption}</p>
+                        <a href="${this.href}">${this.anchorText}</a>
+                    </div>
                 </div>
             </div>
         `
@@ -80,14 +126,18 @@ export class PictureFrame extends LitElement {
      */
     imageRight() {
         return html`
-            <div class='img-container img-container-right'>
-                <div class='frame-item frame-text'>
-                    <p>${this.caption}</p>
-                    <a href="${this.href}">${this.anchorText}</a>
+            <div class='picture-frame-container'>
+                <div class='img-container img-container-right'>
+                    <div class='frame-item frame-text'>
+                        <p>${this.caption}</p>
+                        <a href="${this.href}">${this.anchorText}</a>
+                    </div>
+                    <div class='frame-item frame-image'>
+                        <img src="${this.src}" alt="${this.alt}">
+                        <video src="${this.src}"></video>
+                        <iframe src="${this.src}" frameborder="0"></iframe>
+                    </div>   
                 </div>
-                <div class='frame-item frame-image'>
-                    <img src="${this.src}" alt="${this.alt}">
-                </div>   
             </div>
         `
     }
@@ -97,15 +147,27 @@ export class PictureFrame extends LitElement {
      */
     imageTop() {
         return html`
-
+            <div class='picture-frame-container'>
+                <div class='img-container img-container-top'>
+                    <div class='frame-item frame-image'>
+                        <img src="${this.src}" alt="${this.alt}">
+                        <video src="${this.src}" controls></video>
+                        <iframe width="360" height="202.5" src="${this.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>    
+                    <div class='frame-item frame-text'>
+                        <p>${this.caption}</p>
+                        <a href="${this.href}">${this.anchorText}</a>
+                    </div>
+                </div>
+            </div>
         `
     }
 
     static get properties() {
         return {
-            imgLocation: {
+            displayLocation: {
                 type: String,
-                attribute: 'img-location',
+                attribute: 'display-location',
             },
             src: {
                 type: String,
@@ -121,11 +183,16 @@ export class PictureFrame extends LitElement {
             },
             anchorText: {
                 type: String,
+                attribute: 'anchor-text'
             },
             border: {
                 type: Boolean,
                 reflect: true,
-            }
+            },
+            type: {
+                type: String,
+            },
+
         };
     }
 }

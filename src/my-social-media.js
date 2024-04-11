30e1c73bfd8b6177763f64237f67fcee4ad0dabe
zdodson21 <zdodson21@gmail.com>
2024-04-10 21:17:43 -0400

    created file

---
+++ src/my-social-media.js
@@ -0,0 +1,4 @@
+/**
+ * This would essentially be a web component that will display the user's selected social media icons (chosen using attributes) and when clicked, will take them to their social media page (via link in an attribute)
+ */
+

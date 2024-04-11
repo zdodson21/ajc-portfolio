1655a956fb53e290566244ba0615db8ad71e0e0f
zdodson21 <zdodson21@gmail.com>
2024-04-10 21:35:17 -0400

    listed social medias to make available

--- src/my-social-media.js
+++ src/my-social-media.js
@@ -1,5 +1,44 @@
 /**
  * This would essentially be a web component that will display the user's selected social media icons (chosen using attributes) and when clicked, will take them to their social media page (via link in an attribute)
- * Might make open source (:D)
+ * Might make open source (:D) (so this part is not specifically for the person this portfolio is made for)
  */
 
+
+/* 
+
+https://buffer.com/library/social-media-sites/#23-bluesky-%E2%80%94-mau-unknown
+
+To Implement:
+    * Facebook
+    * Twitter / X
+    * Snapchat
+    * Instagram
+    * GitHub
+    * LinkedIn
+    * Pinterest
+    * Reddit
+    * Discord
+    * Patreon
+    * Amazon
+    * Email
+    * Phone-Number
+    * Steam
+    * Xbox
+    * PlayStation
+    * Threads
+    * TikTok
+    * Twitch
+    * Kick
+    * WhatsApp
+    * Wechat
+    * Telegram
+    * Kuaishou
+    * Sina Weibo
+    * QQ
+    * Onlyfans
+    * Quora
+    * Tumblr
+    * Mastodon
+    * Bluesky
+
+*/

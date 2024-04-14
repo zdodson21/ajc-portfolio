9a8e1e5d336748f7f19f79c82bb1d4295e649ce6
zdodson21 <zdodson21@gmail.com>
2024-04-14 17:50:36 -0400

    temporarily removed anything related to PDF previewing

--- src/picture-frame.js
+++ src/picture-frame.js
@@ -127,7 +127,7 @@ export class PictureFrame extends LitElement {
                         <img class='image' src="${this.src}" alt="${this.alt}">
                         <video class='video' src="${this.src}" controls></video>
                         <iframe class="youtube" width="360" height="202.5" src="${this.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        <iframe class="pdf" width="420" src="${this.src}" frameborder="0"></iframe> 
+                        <!-- <iframe class="pdf" width="420" src="${this.src}" frameborder="0"></iframe>  -->
                         <!-- Implement PDFObject npm -->
                     </div>    
                     <div class='frame-item frame-text'>
@@ -154,7 +154,7 @@ export class PictureFrame extends LitElement {
                         <img class='image' src="${this.src}" alt="${this.alt}">
                         <video class='video' src="${this.src}" controls></video>
                         <iframe class="youtube" width="360" height="202.5" src="${this.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        <iframe class="pdf" src="${this.src}" frameborder="0"></iframe>
+                        <!-- <iframe class="pdf" src="${this.src}" frameborder="0"></iframe> -->
                     </div>   
                 </div>
             </div>

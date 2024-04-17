9b011339867c6a6d4bc0bc7f1ff28de4e34a437d
zdodson21 <zdodson21@gmail.com>
2024-04-17 18:15:05 -0400

    formatted anchor tags for footer

--- src/ajc-footer.js
+++ src/ajc-footer.js
@@ -17,7 +17,14 @@ export class AJCFooter extends LitElement {
 
     static get styles() {
         return css`
-        
+            a:visited, a {
+                color: var(--ajc-color-default-link);
+            }
+
+            a:focus, a:hover {
+                color: var(--ajc-color-default-visited);
+                text-decoration: none;
+            }
         `
     }
 
@@ -25,6 +32,7 @@ export class AJCFooter extends LitElement {
         return html`
             <a href="https://github.com/zdodson21/ajc-portfolio"><img src="" alt="GitHub Logo"></a>
             <a href="https://github.com/zdodson21/ajc-portfolio">GitHub Repository</a>
+            <!-- Write out a notice stating that all art work here belongs to the original owner and is not permitted to be used elsewhere unless otherwise stated -->
         `
     }
 
